aeda73966e463970d81ee847260fd2b16a949390
Karl Rikte <karl.rikte@axis.com>
2022-06-29 14:14:32 +0200

    Refactor whoami.rs to formal lifetime contract
    As a side effect also avoids some memory zeroing and allocation.

--- src/whoami.rs
+++ src/whoami.rs
@@ -1,6 +1,3 @@
-use std::ffi::CStr;
-use std::os::raw::c_char;
-
 pub struct Whoami {
     pub uid: u32,
     pub gid: u32,
@@ -17,42 +14,68 @@ pub fn whoami() -> Whoami {
 }
 
 #[cfg(not(windows))]
+mod unix {
+    pub use std::ffi::CStr;
+    pub use std::mem::MaybeUninit;
+    use std::os::raw::c_char;
+
+    // Safety: C getpwuid_r expects buffers to be uninitialized and the appropriate length
+    const STRING_BUF_LEN: usize = libc::_SC_GETPW_R_SIZE_MAX as usize;
+
+    /// A safe and easy to use zero cost wrapper for libc::passwd.
+    /// Cannot be created outside this module because the inner field is private, so user can't make an invalid one.
+    pub struct SafePasswd<'a>(&'a libc::passwd);
+
+    impl<'a> SafePasswd<'a> {
+        /// calls libc::getpwuid_r but forces params to be correct and enforces the lifetime contract.
+        /// user cant access/move/drop the buffers until return value is dropped.
+        pub fn getpwuid_r(
+            uid: u32,
+            pwd_buf: &'a mut MaybeUninit<libc::passwd>,
+            string_buf: &'a mut MaybeUninit<[c_char; STRING_BUF_LEN]>,
+        ) -> Option<SafePasswd<'a>> {
+            let mut pwd_ptr = MaybeUninit::uninit(); // uninitialized pointer to libc::passwd
+            unsafe {
+                // Safety: getpwuid_r is reentrant and writes only to our buffers here
+                // Safety: pwd_buf is the libc-defined size and shape
+                // Safety: string_buf is the appropriate length and its length is passed
+                let _errno = libc::getpwuid_r(
+                    uid,
+                    pwd_buf.as_mut_ptr(), // ptr to uninit struct
+                    string_buf.as_mut_ptr() as *mut c_char, // ptr to first byte of uninit c_char array
+                    STRING_BUF_LEN,
+                    pwd_ptr.as_mut_ptr(), // ptr to uninit ptr to libc::passwd. *result is always written.
+                );
+                // Safety: by contract with getpwuid_r, pwd_ptr is now either null or a valid pointer to an initialized pwd_buf
+                // Safety: if pwd_ptr non-null, C string pointers point somewhere valid inside string_buf.
+                pwd_ptr.assume_init().as_ref()
+            }
+            // Safety: return value borrows both buffers that back it. User can't make it invalid without using unsafe.
+            .map(|c| SafePasswd(c))
+        }
+
+        /// safe access to pw_name
+        pub fn pw_name(&'a self) -> &'a CStr {
+            // see read why this is always safe.
+            unsafe { CStr::from_ptr(self.0.pw_name) }
+        }
+    }
+}
+
+#[cfg(not(windows))]
 pub fn whoami() -> Whoami {
     let uid = unsafe { libc::geteuid() };
     let gid = unsafe { libc::getegid() };
-    let username = {
-        // Safety: C expects this buffer to be uninitialized
-        let mut pwd_buf: libc::passwd = unsafe { std::mem::zeroed() };
-        let mut string_buf = vec![0 as c_char; libc::_SC_GETPW_R_SIZE_MAX as usize];
-        let mut pwd_ptr = std::ptr::null_mut::<libc::passwd>();
 
-        // Safety: getpwuid_r is reentrant and writes only to our buffers here
-        // pwd_buf is the libc-defined size and shape
-        // string_buf is the appropriate length and its length is passed
-        let _errno = unsafe {
-            libc::getpwuid_r(
-                uid,
-                &mut pwd_buf as *mut libc::passwd,
-                string_buf.as_mut_ptr(),
-                string_buf.len(),
-                &mut pwd_ptr as *mut *mut libc::passwd,
-            );
-        };
-
-        // Safety: by contract with getpwuid_r, pwd_ptr is either null or a valid pointer to pwd_buf
-        unsafe { pwd_ptr.as_ref() }
-            // If we have a passwd struct, get the pw_name field
-            .map(|pwd| pwd.pw_name)
-            // Safety: pwd.pw_name points to a position within string_buf which is valid through the
-            // end of this block
-            .map(|ptr| unsafe { CStr::from_ptr(ptr) })
-            // If we have a name, copy (force) it into a String
-            .map(|cstr| cstr.to_string_lossy().into_owned())
-    };
+    use unix::*;
+    let mut pwd_buf = MaybeUninit::uninit();
+    let mut string_buf = MaybeUninit::uninit();
+    let opt_safe_pwd = SafePasswd::getpwuid_r(uid, &mut pwd_buf, &mut string_buf);
+    // pwd_buf and string_buf are now borrowed by opt_safe_pwd and we can't violate any contract.
 
     Whoami {
         uid: uid as _,
         gid: gid as _,
-        username,
+        username: opt_safe_pwd.map(|pwd| pwd.pw_name().to_string_lossy().to_string()),
     }
 }
